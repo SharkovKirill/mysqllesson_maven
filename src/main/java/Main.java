d4720fa6659b6c2023b4b573651e505c1ff13a4a
SharkovKirill <kirill.office75@gmail.com>
2021-11-18 12:28:46 +0700

    all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        SQLdb.updateDB("21/09/2021");
-        SQLdb.check("20/09/2021", "AMD");
+        if(args[0].equals("updateDB")){
+            SQLdb.updateDB(parse(args[1]));
+        } else {
+            SQLdb.check(parse(args[0]), args[1]);
+        }
+
+    }
+    public static String parse(String date){
+        String[] subStr;
+        String delimeter = "-";
+        subStr = date.split(delimeter);
+        String newDate = subStr[2]+"/"+subStr[1]+"/"+subStr[0];
+        System.out.println(newDate);
+        return newDate;
     }
 }
